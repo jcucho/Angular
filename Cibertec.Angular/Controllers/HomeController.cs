8c9c12bde1d8ee27c41e55cce88664a72631d173
Juan Cucho <juan.cucho@outlook.com>
2017-11-10 19:16:26 -0500

    Agregar archivos de proyecto.

---
+++ Cibertec.Angular/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Cibertec.Angular.Code;
+using Microsoft.Extensions.Options;
+
+namespace Cibertec.Angular.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ConfigurationFile _config;
+        public HomeController(IOptions<ConfigurationFile> config)
+        {
+            _config = config.Value;
+        }        
+
+        public IActionResult Index()
+        {
+            ViewBag.WebApiUrl = _config.WebApiUrl;
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+    }
+}
